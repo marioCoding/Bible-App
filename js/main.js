a488e96354416a730a3984d692b99ea11f4b9310
marioCoding <marosw.sal@gmail.com>
2023-06-06 12:55:59 -0700

    removed js javascript. added code to fetch API

--- js/main.js
+++ js/main.js
@@ -1,48 +1,60 @@
-const daysTag = document.querySelector(".days"),
-currentDate = document.querySelector(".current-date"),
-prevNextIcon = document.querySelectorAll(".icons span");
-// getting new date, current year and month
-let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
-// storing full name of all months in array
-const months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
-const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
-    let liTag = "";
-    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
-        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-    }
-    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
-        // adding active class to li if the current day, month, and year matched
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
-                     && currYear === new Date().getFullYear() ? "active" : "";
-        liTag += `<li class="${isToday}">${i}</li>`;
-    }
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
-        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
-    }
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
-    daysTag.innerHTML = liTag;
+const dateControl = document.querySelector('input[type="date"]');
+//const date = document.getElementById('datePicker').valueAsDate = new Date();
+const container = document.querySelector(".article2");
+let currentDate = document.querySelector('.current-date');
+
+async function getData() {
+  // grab calender value
+  
+  // fetch api
+  let url = "http://calapi.inadiutorium.cz/calenders/general-en/today";
+  let response = await fetch(url);
+  let calender = await response.json();
+  try {
+    const dateElement = document.createElement("h2");
+    const seasonElement = document.createElement("p");
+    const seasonWeekElement = document.createElement("p");
+    const celebrationsTitle1Element = document.createElement("p");
+    const celebrationsColor1Element = document.createElement("p");
+    const celebrationsTitle2Element = document.createElement("p");
+    const celebrationsColor2Element = document.createElement("p");
+    const weekdayElement = document.createElement("p"); 
+    // convert JSON data into objects
+    let dateData = await calender.date;
+    let seasonData = await calender.season;
+    let seasonWeekData = await calender.season_week;
+    let celebrationsTitle1Data = await calender.celebrations[0].title;
+    let celebrationsColor1Data = await calender.celebrations[0].colour;
+    let celebrationsTitle2Data = await calender.celebrations[1].title;
+    let celebrationsColor2Data = await calender.celebrations[1].colour;
+    let weekdayData = await calender.weekday;
+    // give the html elements' text content the JSON objects
+    dateElement.textContent = dateData;
+    seasonElement.textContent= seasonData;
+    seasonWeekElement.textContent = seasonWeekData;
+    celebrationsTitle1Element.textContent = celebrationsTitle1Data;
+    celebrationsColor1Element.textContent = celebrationsColor1Data;
+    celebrationsTitle2Element.textContent = celebrationsTitle2Data;
+    celebrationsColor2Element.textContent = celebrationsColor2Data;
+    weekdayElement.textContent = weekdayData;
+
+
+    // make reference to the html containers where the info 
+    // will be displayed
+    container.innerHTML = " ";
+    container.appendChild(dateElement);
+    container.appendChild(seasonElement);
+    container.appendChild(seasonWeekElement);
+    container.appendChild(celebrationsTitle1Element);
+    container.appendChild(celebrationsColor1Element);
+    container.appendChild(celebrationsTitle2Element);
+    container.appendChild(celebrationsColor2Element);
+    container.appendChild(weekdayElement);
+
+    
+  } catch {
+    err => console.error("Oops!", err.message);
+  }
 }
-renderCalendar();
-prevNextIcon.forEach(icon => { // getting prev and next icons
-    icon.addEventListener("click", () => { // adding click event on both icons
-        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
-        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as date value
-            date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // updating current year with new date year
-            currMonth = date.getMonth(); // updating current month with new date month
-        } else {
-            date = new Date(); // pass the current date as date value
-        }
-        renderCalendar(); // calling renderCalendar function
-    });
-});
-33
+
+// To do: grab calender value and pass it into the API url
